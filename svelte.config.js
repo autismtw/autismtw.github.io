7943da9dddc8886c5bbd1f3c5c80a8d7c465c443
Madeline Sparkles <madeline@mouse.lgbt>
2024-01-04 22:02:09 +0800

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,9 @@ import { mdsvex } from 'mdsvex'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            fallback: '404.html',
+        }),
         paths: {
             base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
         },
