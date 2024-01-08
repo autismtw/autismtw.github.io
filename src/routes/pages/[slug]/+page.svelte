c2630010eaa0924165c4426fd70482167e51b656
Madeline Sparkles <madeline@mouse.lgbt>
2024-01-08 17:57:32 +0800

    upate

--- src/routes/pages/[slug]/+page.svelte
+++ src/routes/pages/[slug]/+page.svelte
@@ -15,18 +15,18 @@
         }
     }
 
-    function formatWithColon(s: string) {
+    function formatWithColon(s?: string) {
         return s ? `：${s}` : ''
     }
 </script>
 
 <section>
     <nav>
-        <a href={data.prev.path}>上一篇{formatWithColon(data.prev.metadata?.title)}</a>
+        <a href={data.prev?.path}>上一篇{formatWithColon(data.prev?.metadata?.title)}</a>
 
         <p>正在顯示：第 <code>{data.ord}</code> 篇</p>
 
-        <a href={data.next.path}>下一篇{formatWithColon(data.next.metadata?.title)}</a>
+        <a href={data.next?.path}>下一篇{formatWithColon(data.next?.metadata?.title)}</a>
     </nav>
 </section>
 
--- src/routes/pages/[slug]/+page.ts
+++ src/routes/pages/[slug]/+page.ts
@@ -2,20 +2,18 @@ import type { ComponentType } from 'svelte'
 import { _BASE } from '../../+page'
 
 export async function load({ fetch, params }) {
-    const post = await import(`../${params.slug}.md`)
-    const content = post.default as ComponentType
+    const posts = (await (await fetch(`${_BASE}/api/posts`)).json()) as {
+        metadata: PageMeta
+        path: string
+    }[]
 
+    const post = await import(`../${params.slug}.md`)
+
+    const content = post.default as ComponentType
     const metadata = post.metadata as PageMeta
 
-    const next = (await (await fetch(`${_BASE}/api/next?from=${metadata.ord}`)).json()) as {
-        metadata: PageMeta
-        path: string
-    }
-
-    const prev = (await (await fetch(`${_BASE}/api/prev?from=${metadata.ord}`)).json()) as {
-        metadata: PageMeta
-        path: string
-    }
+    const next = posts.find((p) => p.metadata.ord > metadata.ord)
+    const prev = posts.findLast((p) => p.metadata.ord < metadata.ord)
 
     return {
         next,
